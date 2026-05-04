0eafe5216d2cace6f8da56a508f1bfebf20db393
1122414 <758370266@qq.com>
2026-05-04 17:18:09 +0800

    fix: 修复SearchMixin缩进错误导致app无法启动

--- app/ui/search_mixin.py
+++ app/ui/search_mixin.py
@@ -22,6 +22,7 @@ class SearchMixin:
 
     def _do_search(self):
         keyword = self._last_search_keyword
+        if not keyword:
             self._clear_search_results()
             return
         search_id, worker = search_service.search_async(keyword, config.search_case_insensitive)
